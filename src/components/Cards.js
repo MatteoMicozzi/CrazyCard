a475ffcec3b03b44258cbaf9a8465b8b3fe00d1f
MatteoMicozzi <micozzimatteo@yahoo.it>
2022-04-05 22:30:17 +0100

    Added cards available in Cards component.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,26 @@
-const cards = []
+const cards = [
+  {
+    cardType: 'Student Life',
+    aprPercentage: 18.9,
+    balanceTransferOfferMonthsDuration: 0,
+    purchaseOfferMonthsDuration: 6,
+    poundsCreditAvailable: 1200
+  }
+  {
+    cardType: 'Anywhere Card',
+    aprPercentage: 33.9,
+    balanceTransferOfferMonthsDuration: 0,
+    purchaseOfferMonthsDuration: 0,
+    poundsCreditAvailable: 300
+  }
+  {
+    cardType: 'Liquid Card',
+    aprPercentage: 33.9,
+    balanceTransferOfferMonthsDuration: 12,
+    purchaseOfferMonthsDuration: 6,
+    poundsCreditAvailable: 3000
+  }
+]
 
 const Cards = () => {
   return (
