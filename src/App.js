425b3005f980e6b42cada2fa6330a472a99f9827
MatteoMicozzi <micozzimatteo@yahoo.it>
2022-04-05 22:54:50 +0100

    Moved cards from Cards component to App to be accessible anywhere, plus added id & card selection.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import { useState } from 'react'
 import Header from './components/Header'
 
 function App() {
+  const [cards] = useState([
+    {
+      id: 1,
+      cardType: 'Student Life',
+      aprPercentage: 18.9,
+      balanceTransferOfferMonthsDuration: 0,
+      purchaseOfferMonthsDuration: 6,
+      poundsCreditAvailable: 1200,
+      selected: false
+    },
+    {
+      id: 2,
+      cardType: 'Anywhere Card',
+      aprPercentage: 33.9,
+      balanceTransferOfferMonthsDuration: 0,
+      purchaseOfferMonthsDuration: 0,
+      poundsCreditAvailable: 300,
+      selected: false
+    },
+    {
+      id: 3,
+      cardType: 'Liquid Card',
+      aprPercentage: 33.9,
+      balanceTransferOfferMonthsDuration: 12,
+      purchaseOfferMonthsDuration: 6,
+      poundsCreditAvailable: 3000,
+      selected: false
+    }
+  ])
+
   return (
     <div className="App">
       <Header />
