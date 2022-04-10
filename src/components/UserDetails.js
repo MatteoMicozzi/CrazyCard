808468e33cc666a72c73476532b46e953171f2db
MatteoMicozzi <micozzimatteo@yahoo.it>
2022-04-10 09:43:16 +0100

    Used state in the userDetails component in order to use them to filter the cards available for a certain user.

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,35 +1,76 @@
+import { useState } from 'react'
+
 const UserDetails = () => {
+  const [titleName, setTitleName] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [dateOfBirth, setDateOfBirth] = useState('')
+  const [status, setStatus] = useState('')
+  const [income, setIncome] = useState('')
+  const [houseNumber, setHouseNumber] = useState('')
+  const [postcode, setPostcode] = useState('')
+
   return (
     <form className='submit-form'>
       <div className='form-row'>
         <div className='form-name'>
           <div className='title-name'>
             <label>Title Name</label>
-            <input type='text' placeholder='Add Title Name' />
+            <input
+              type='text'
+              placeholder='Add Title Name'
+              value={titleName}
+              onChange={(e) => setTitleName(e.target.value)}
+            />
           </div>
           <div className='first-name'>
             <label>First Name</label>
-            <input type='text' placeholder='Add First Name' />
+            <input
+              type='text'
+              placeholder='Add First Name'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
           <div className='last-name'>
             <label>Last Name</label>
-            <input type='text' placeholder='Add Last Name' />
+            <input
+              type='text'
+              placeholder='Add Last Name'
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
           </div>
         </div>
       </div>
       <div className='form-row'>
         <label>Date of Birth</label>
-        <input type='text' placeholder='DD/MM/YYYY' />
+        <input
+          type='text'
+          placeholder='DD/MM/YYYY'
+          value={dateOfBirth}
+          onChange={(e) => setDateOfBirth(e.target.value)}
+        />
       </div>
       <div className='form-row'>
         <div className='form-finance'>
           <div className='employment-status'>
             <label>Employment Status</label>
-            <input type='text' placeholder='Add Employment Status' />
+            <input
+              type='text'
+              placeholder='Add Employment Status'
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+            />
           </div>
           <div className='annual-income'>
             <label>Annual Income</label>
-            <input type='text' placeholder='Add Annual Income' />
+            <input
+              type='text'
+              placeholder='Add Annual Income'
+              value={income}
+              onChange={(e) => setIncome(e.target.value)}
+            />
           </div>
         </div>
       </div>
@@ -37,11 +78,21 @@ const UserDetails = () => {
         <div className='form-address'>
           <div className='house-number'>
             <label>House Number</label>
-            <input type='text' placeholder='Add House Number' />
+            <input
+              type='text'
+              placeholder='Add House Number'
+              value={houseNumber}
+              onChange={(e) => setHouseNumber(e.target.value)}
+            />
           </div>
           <div className='postcode'>
             <label>Postcode</label>
-            <input type='text' placeholder='Add Postcode' />
+            <input
+              type='text'
+              placeholder='Add Postcode'
+              value={postcode}
+              onChange={(e) => setPostcode(e.target.value)}
+            />
           </div>
         </div>
       </div>
